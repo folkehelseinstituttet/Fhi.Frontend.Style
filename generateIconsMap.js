a5480319cfbe5b5085b70f7be3ec3ef95d393801
Karl Granli <karl@gran.li>
2021-11-22 16:08:45 +0100

    To be reviewed
    Removed icons not used for the time being
    Generate map ordered alphabetically by name

--- generateIconsMap.js
+++ generateIconsMap.js
@@ -4,17 +4,17 @@ const path = require('path');
 const iconsPath = './src/icons/svg-icons/';
 const iconsMapOutput = './src/icons/_generated-icons-map.scss';
 
-let totalNumberOfIcons = 0;
 let iconsCounter = 0;
+const svgNameAndContent = [];
 
 
 const generateIconsSASS = function() {
-  // fs.writeFile(iconsMapOutput, '$icons:(', error => {
-  //   if (error) {
-  //     console.error(error);
-  //     return;
-  //   }
-  // });
+  fs.writeFile(iconsMapOutput, '$icons:(', error => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+  });
 
   getTheFiles(iconsPath);
 };
@@ -22,15 +22,17 @@ const generateIconsSASS = function() {
 
 const getTheFiles = function(dirPath, arrayOfFiles) {
   const files = fs.readdirSync(dirPath);
+  const numberOfFilesInDir = files.length;
 
+  let fileCounter = 0;
   arrayOfFiles = arrayOfFiles || [];
 
-  files.forEach(function(file, index) {
+  files.forEach(function(file) {
     let currentFile = path.join(__dirname, dirPath, '/', file);
     if (fs.statSync(dirPath + '/' + file).isDirectory()) {
       arrayOfFiles = getTheFiles(dirPath + '/' + file, arrayOfFiles);
     } else if (path.parse(currentFile).ext === '.svg') {
-      arrayOfFiles.push(currentFile);
+      fileCounter ++;
 
       fs.readFile(currentFile, { encoding: 'utf-8' }, (error, data) => {
         if (error) {
@@ -38,52 +40,67 @@ const getTheFiles = function(dirPath, arrayOfFiles) {
           return;
         }
 
+        const currentSVG = [];
+
         if (path.parse(currentFile).ext === '.svg') {
           // removing all kinds of newlines
           data = data.replace(/(\r\n|\n|\r)/gm, '');
-          // const content = `'${path.parse(arrayOfFiles[index]).name}':'${data}',\n`;
-          const content = `['${path.parse(currentFile).name}', '${data}']`;
-          // fs.appendFile(iconsMapOutput, content, 'utf-8', closeTheMap, error => {
-          //   if (error) {
-          //     console.error(error);
-          //     return;
-          //   }
-          // });
 
-          arrayOfFiles[index] = content;
+          currentSVG.push(`${path.parse(currentFile).name}`);
+          currentSVG.push(`${data}`);
+
+          svgNameAndContent.push(currentSVG);
 
           iconsCounter ++;
+
+          if (iconsCounter === totalNumberOfIcons) {
+            svgNameAndContent.sort(sortSVGName);
+
+            function printList(i) {
+              setTimeout(function() {
+                if (i < svgNameAndContent.length) {
+                  const content = `'${svgNameAndContent[i][0]}':'${svgNameAndContent[i][1]}',\n`;
+                  fs.appendFile(iconsMapOutput, content, 'utf-8', error => {
+                    if (error) {
+                      console.error(error);
+                      return;
+                    }
+                  });
+                  i ++;
+                  printList(i);
+                } else {
+                  closeTheMap();
+                }
+              }, 25);// a small timeout makes sure all icons gets generated correctly
+            }
+            printList(0);
+          }
         }
       });
     };
   });
-
-  // console.info(arrayOfFiles);
 };
 
 
 const closeTheMap = function() {
-  if (iconsCounter === totalNumberOfIcons) {
-    fs.appendFile(iconsMapOutput, ');\n', 'utf8', error => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-      console.info('\n * * * $icons map is generated * * *\n');
-    });
-    iconsCounter ++;
-  }
+  fs.appendFile(iconsMapOutput, ');\n', 'utf8', error => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.info(' * * * [' + iconsMapOutput + '] is generated * * *\n');
+  });
 }
 
 
-const getNumberOfFiles = function(dirPath, svgCount) {
+const getNumberOfSVGs = function(dirPath, svgCount) {
   files = fs.readdirSync(dirPath);
 
   svgCount = svgCount || 0;
 
   files.forEach(function(file) {
     if (fs.statSync(dirPath + '/' + file).isDirectory()) {
-      svgCount = getNumberOfFiles(dirPath + '/' + file, svgCount);
+      svgCount = getNumberOfSVGs(dirPath + '/' + file, svgCount);
     } else if (path.parse(file).ext === '.svg') {
       svgCount ++;
     }
@@ -91,9 +108,17 @@ const getNumberOfFiles = function(dirPath, svgCount) {
 
   return svgCount;
 }
+const totalNumberOfIcons = getNumberOfSVGs(iconsPath);
+console.info('\n * * * ' + totalNumberOfIcons + ' SVG icons are found in ' + iconsPath);
 
 
-totalNumberOfIcons = getNumberOfFiles(iconsPath);
-console.info('\n' + totalNumberOfIcons + ' SVG icons are found in ' + iconsPath);
+function sortSVGName(a, b) {
+  if (a[0] === b[0]) {
+    return 0;
+  } else {
+    return (a[0] < b[0]) ? -1 : 1;
+  }
+}
+
 
 generateIconsSASS();
